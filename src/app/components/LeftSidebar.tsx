6c592a029e6e810e927948c97f5869b6e2dec943
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-07-20 17:55:37 -0500

    ui: left side bar

--- src/app/components/LeftSidebar.tsx
+++ src/app/components/LeftSidebar.tsx
@@ -45,24 +45,33 @@ const NAVIGATION_ITEMS = [
 
 export default function LeftSidebar() {
   return (
-    <section className="w-72 h-screen ">
-      <div className="flex flex-col gap-y-4">
-        <div>
+    <section className="w-72 h-screen flex flex-col">
+      <div className="flex flex-col gap-y-4 items-stretch h-screen">
+        <div className="py-2 px-4 text-3xl">
           <FaTwitter />
         </div>
         {NAVIGATION_ITEMS.map((item) => (
-          <Link key={item.title} href={item.title.toLowerCase()}>
+          <Link
+            key={item.title}
+            href={item.title.toLowerCase()}
+            className="dark:hover:bg-gray-50 dark:hover:text-black hover:bg-gray-200 transition duration-200 px-4 py-2 rounded-full"
+          >
             <div className="flex items-center gap-x-4">
               <item.icon /> <span>{item.title}</span>
             </div>
           </Link>
         ))}
-        <div className="w-100">
-          <button className="w-100 rounded-2xl py-4 text-center bg-blue-500">
-            Tweet
-          </button>
-        </div>
+        <button className="w-100 rounded-full mx-4 py-4 text-center bg-blue-500 text-white hover:bg-opacity-80">
+          Tweet
+        </button>
       </div>
+      <button className="rounded-full flex items-center gap-x-2 m-4 p-4 bg-transparent text-center dark:hover:bg-white/10 hover:bg-gray-200 transition duration-200">
+        <div className="rounded-full bg-slate-400 w-8 h-8"></div>
+        <div className="text-left text-xs">
+          <div className="font-semibold">Profile</div>
+          <div className="dark:text-gray-400 text-gray-600">@user_handle</div>
+        </div>
+      </button>
     </section>
   );
 }

a051986695ee528ab62441e5db1c0b3e6f7e7e23
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-07-21 15:37:48 -0500

    ui: SuggestedProphets component

---
+++ src/app/components/right-sidebar/Prophet.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+interface Prophet {
+  username: string;
+  handle: string;
+}
+
+export default function Prophet(prophet: Prophet) {
+  return (
+    <div className="flex items-center justify-between w-full">
+      <div className="flex gap-x-4">
+        <div
+          data-testid="avatar"
+          className="w-10 h-10 bg-slate-500 rounded-full"
+        ></div>
+        <div className="flex flex-col">
+          <div className="font-semibold">{prophet.username}</div>
+          <div className="text-gray-500">
+            <span
+              dangerouslySetInnerHTML={{
+                __html: "&#64",
+              }}
+            ></span>
+            {prophet.handle}
+          </div>
+        </div>
+      </div>
+      <button className="rounded-full dark:bg-white dark:text-black bg-black text-white py-2 px-4 font-semibold">
+        Follow
+      </button>
+    </div>
+  );
+}
--- src/app/components/right-sidebar/SuggestedProphets.tsx
+++ src/app/components/right-sidebar/SuggestedProphets.tsx
@@ -1,14 +1,36 @@
 import React from "react";
+import Prophet from "./Prophet";
+
+interface Prophet {
+  username: string;
+  handle: string;
+}
+
+const PROPHETS: Prophet[] = [
+  { username: "Alice", handle: "alice" },
+  { username: "Bob", handle: "bob" },
+  { username: "Foo", handle: "foo" },
+];
 
 export default function SuggestedProphets() {
   return (
-    <div className="rounded-2xl overflow-hidden flex flex-col dark:bg-white/10 bg-black/10">
+    <div className="rounded-2xl overflow-hidden flex flex-col dark:bg-white/10 bg-silver">
       <div className="p-3">
         <h2 className="text-lg font-bold">You might like</h2>
       </div>
-      {/* TODO: Create Prophet component */}
-      <div data-testid="prophets"></div>
-      <div className="p-3 dark:text-blue-500 text-blue-700 dark:hover:bg-white/10  hover:bg-black/10 cursor-pointer">
+      <div data-testid="prophets" className="flex flex-col gap-y-4 px-3 py-2">
+        {PROPHETS.map((prophet) => (
+          <Prophet
+            key={prophet.username}
+            username={prophet.username}
+            handle={prophet.handle}
+          />
+        ))}
+      </div>
+      <div
+        role="button"
+        className="p-3 dark:text-blue-500 text-blue-700 dark:hover:bg-white/10  hover:bg-black/10 cursor-pointer"
+      >
         Show more
       </div>
     </div>

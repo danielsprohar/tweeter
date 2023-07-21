1f075111279976a6a5eb9c5edea1511440b2a59c
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-07-21 15:37:29 -0500

    Added a key to the Tweets in the list

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <>
       {Array.from({ length: 5 }).map((_, i) => (
-        <Tweet index={i} />
+        <Tweet key={i} index={i} />
       ))}
     </>
   );

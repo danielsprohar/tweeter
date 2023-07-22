6486118fc94aa3d645713bd6d6cbe040e14f3986
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-07-22 15:32:35 -0500

    ui: padding and spacing to the AccountMenu

--- src/app/components/left-sidebar/AccountMenu.tsx
+++ src/app/components/left-sidebar/AccountMenu.tsx
@@ -6,17 +6,17 @@ export default function AccountMenu() {
     <div
       aria-label="Account menu"
       role="button"
-      className="flex items-center p-2 rounded-full w-full dark:hover:bg-white/10 hover:bg-black/10 transition duration-200"
+      className="flex items-center justify-between px-4 py-2 rounded-full w-full dark:hover:bg-white/10 hover:bg-black/10 transition duration-200"
     >
       <div className="flex gap-x-2">
         {/* User avatar */}
         <div className="w-10 h-10 rounded-full bg-slate-300"></div>
 
         <div className="flex flex-col">
-          <div className="font-bold">Name</div>
+          <div className="font-bold">John Doe</div>
           <div className="font-gray-500">
             <span dangerouslySetInnerHTML={{ __html: '&#64;' }}></span>
-            username
+            johndoe
           </div>
         </div>
       </div>

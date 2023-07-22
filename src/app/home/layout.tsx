f0e75ed0794a63ffb16aa88191b1e2934784984a
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-07-22 09:20:29 -0500

    Restructured the project layout

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,9 +1,11 @@
-import { Metadata } from "next";
-import React from "react";
-import ComposeTweet from "../components/ComposeTweet";
+import { Metadata } from 'next';
+import React from 'react';
+import ComposeTweet from '../components/ComposeTweet';
+import LeftSidebar from '../components/LeftSidebar';
+import RightSidebar from '../components/right-sidebar/RightSidebar';
 
 export const metadata: Metadata = {
-  title: "Home | Tweeter",
+  title: 'Home | Tweeter',
 };
 
 export default function HomeLayout({
@@ -13,9 +15,13 @@ export default function HomeLayout({
 }) {
   return (
     <>
-      <h1 className="text-xl mb-4 font-semibold">Home</h1>
-      <ComposeTweet />
-      <div>{children}</div>
+      <LeftSidebar />
+      <main className="p-2">
+        <h1 className="text-xl mb-4 font-semibold">Home</h1>
+        <ComposeTweet />
+        <div>{children}</div>
+      </main>
+      <RightSidebar />
     </>
   );
 }
---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import AuthForm from '../components/auth/auth-form';
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <AuthForm />
+    </div>
+  );
+}

52f433bab399be7f3efa860031e2801cedb7c7d0
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-07-23 13:08:34 -0500

    ui: changed the color of the Login button

--- src/app/components/auth/auth-form.tsx
+++ src/app/components/auth/auth-form.tsx
@@ -1,22 +1,35 @@
-'use client';
+'use client'
 
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { Auth } from '@supabase/auth-ui-react';
-import { ThemeSupa } from '@supabase/auth-ui-shared';
-import { Database } from '../../../../types/supabase';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { Database } from '../../../../types/supabase'
 
 export default function AuthForm() {
-  const supabase = createClientComponentClient<Database>();
+  const supabase = createClientComponentClient<Database>()
+  const tweeterBlueHexColor = '#1D9BF0'
 
   return (
     <Auth
       supabaseClient={supabase}
       view="magic_link"
-      appearance={{ theme: ThemeSupa }}
-      theme="dark"
+      appearance={{
+        theme: ThemeSupa,
+        variables: {
+          default: {
+            colors: {
+              brand: tweeterBlueHexColor,
+              defaultButtonBackground: tweeterBlueHexColor,
+              defaultButtonText: '#ffffff',
+              defaultButtonBackgroundHover: tweeterBlueHexColor,
+            },
+          },
+        },
+      }}
+      socialLayout={'vertical'}
       showLinks={false}
       providers={[]}
       redirectTo="http://localhost:3000/auth/callback"
     />
-  );
+  )
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,10 @@
-import AuthForm from '../components/auth/auth-form';
+import AuthForm from '../components/auth/auth-form'
 
 export default function LoginPage() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex flex-col h-screen items-center justify-center w-full">
+      <h1 className="text-3xl font-semibold">Sign in</h1>
       <AuthForm />
     </div>
-  );
+  )
 }

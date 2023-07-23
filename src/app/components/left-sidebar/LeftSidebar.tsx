99854b452d6002801eabcf5ab045997eb7f4a04d
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-07-22 23:23:52 -0500

    ui: bold font for the active nav link

--- src/app/components/left-sidebar/LeftSidebar.tsx
+++ src/app/components/left-sidebar/LeftSidebar.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { BiSolidHomeCircle } from 'react-icons/bi'
 import {
   FaBell,
@@ -15,52 +18,67 @@ import AccountMenu from './AccountMenu'
 const NAVIGATION_ITEMS = [
   {
     title: 'Home',
+    path: '/home',
     icon: BiSolidHomeCircle,
   },
   {
     title: 'Explore',
+    path: '/explore',
     icon: FaHashtag,
   },
   {
     title: 'Notifications',
+    path: '/notifications',
     icon: FaBell,
   },
   {
     title: 'Messages',
+    path: '/messages',
     icon: FaEnvelope,
   },
   {
     title: 'Bookmarks',
+    path: '/bookmarks',
     icon: FaBookmark,
   },
   {
     title: 'Profile',
+    path: '/profile',
     icon: FaUser,
   },
   {
     title: 'More',
+    path: '/more',
     icon: CgMoreO,
   },
 ]
 
 export default function LeftSidebar() {
+  const linkClass = 'dark:hover:bg-white/10 hover:bg-black/10 transition duration-200 px-4 py-2 rounded-full text-lg'
+  const path = usePathname();
+
   return (
     <section className="fixed w-72 h-screen flex flex-col border-r border-black/10 dark:border-white/10">
       <div className="flex flex-col gap-y-4 items-stretch h-screen">
         <div className="py-2 px-4 text-3xl">
           <FaTwitter />
         </div>
-        {NAVIGATION_ITEMS.map((item) => (
-          <Link
-            key={item.title}
-            href={item.title.toLowerCase()}
-            className="dark:hover:bg-white/10 hover:bg-black/10 transition duration-200 px-4 py-2 rounded-full text-lg"
-          >
-            <div className="flex items-center gap-x-4">
-              <item.icon /> <span>{item.title}</span>
-            </div>
-          </Link>
-        ))}
+        {NAVIGATION_ITEMS.map((item) => {
+          const isActive = path.startsWith(item.path)
+          console.log('path', path, 'item.path', item.path)
+
+          return (
+            <Link
+              key={item.title}
+              href={item.path}
+              className={ isActive ? `${linkClass} font-bold` : linkClass}
+            >
+              <div className="flex items-center gap-x-4">
+                <item.icon /> <span>{item.title}</span>
+              </div>
+            </Link>
+          )
+        })}
         <button className="w-100 rounded-full mx-4 py-4 text-center bg-tweeter-blue text-white hover:bg-opacity-80 font-semibold">
           Tweet
         </button>

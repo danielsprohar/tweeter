4f6302ee00c7b5341ffd2f4bd3223e7949675e5f
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-07-24 10:19:01 -0500

    enum: emoji category

---
+++ src/app/enums/emoji-category.ts
@@ -0,0 +1,10 @@
+export enum EmojiCategory {
+  SMILEYS_AND_PEOPLE = 'Smileys & people',
+  ANIMALS_AND_NATURE = 'Animals & nature',
+  FOOD_AND_DRINK = 'Food & drink',
+  ACTIVITY = 'Activity',
+  TRAVEL_AND_PLACES = 'Travel & places',
+  OBJECTS = 'Objects',
+  SYMBOLS = 'Symbols',
+  FLAGS = 'Flags',
+}

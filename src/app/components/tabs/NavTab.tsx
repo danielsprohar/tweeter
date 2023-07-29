9c3176cfa4bd55e94d28440c13b1394709659eac
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-07-29 15:44:24 -0500

    ui: html semantics for the NavTabs & NavTab components

--- src/app/components/tabs/NavTab.tsx
+++ src/app/components/tabs/NavTab.tsx
@@ -9,27 +9,22 @@ export interface NavTabProps {
   onClick?: (event: any) => void
 }
 
-export function NavTab({
-  href,
-  displayText,
-  isActive,
-  onClick,
-}: NavTabProps) {
+export function NavTab({ href, displayText, isActive, onClick }: NavTabProps) {
   return (
-    <div className="cursor-pointer w-full flex flex-1 justify-center font-semibold dark:hover:bg-white/10 hover:bg-black/10">
+    <li className="cursor-pointer w-full flex flex-1 justify-center font-semibold dark:hover:bg-white/10 hover:bg-black/10">
       <Link href={href} onClick={onClick} className="w-full">
         <div className="flex justify-center items-center">
           <div
             className={
               isActive
-                ? 'border-b-4 border-tweeter-blue py-4'
-                : 'text-gray-500 py-4'
+                ? 'border-b-4 border-tweeter-blue py-4 px-2'
+                : 'text-gray-500 py-4 px-2'
             }
           >
             {displayText}
           </div>
         </div>
       </Link>
-    </div>
+    </li>
   )
 }
--- src/app/components/tabs/NavTabs.tsx
+++ src/app/components/tabs/NavTabs.tsx
@@ -4,8 +4,8 @@ import { Children } from '../../types/children'
 
 export default function NavTabs({ children }: Children) {
   return (
-    <div className="flex items-center h-full overflow-y-hidden overflow-x-auto">
-      {children}
-    </div>
+    <nav className="h-full overflow-y-hidden overflow-x-auto border-b border-black/10 dark:border-white/10">
+      <ul className="list-none m-0 p-0 flex items-center">{children}</ul>
+    </nav>
   )
 }

7db9049b87266bd444d992913ff4de4a8756f978
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-07-29 10:31:43 -0500

    ui: changed the border color & adjusted the height of the RightSidebar

--- src/app/components/right-sidebar/RightSidebar.tsx
+++ src/app/components/right-sidebar/RightSidebar.tsx
@@ -4,7 +4,7 @@ import SuggestedProphets from './SuggestedProphets'
 
 export default function RightSidebar() {
   return (
-    <section className="flex flex-col gap-y-4 p-4 border-l">
+    <section className="h-screen flex flex-col gap-y-4 p-4 border-l border-black/10 dark:border-white/10">
       <SearchTwitter />
       <StatusGrid />
       <SuggestedProphets />

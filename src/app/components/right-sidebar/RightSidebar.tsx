8cbcc5eccc24b1e4faf61c6b7cc9bf5b2961a4db
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-07-30 12:11:21 -0500

    ui: gave the RightSidebar a width of 350px on medium+ devices

--- src/app/components/right-sidebar/RightSidebar.tsx
+++ src/app/components/right-sidebar/RightSidebar.tsx
@@ -2,7 +2,7 @@ import { Children } from '../../types/children'
 
 export default function RightSidebar({ children }: Children) {
   return (
-    <section className="h-screen flex flex-col gap-y-4 p-4 border-l border-black/10 dark:border-white/10">
+    <section className="h-screen md:w-[350px] flex flex-col gap-y-4 p-4 border-l border-black/10 dark:border-white/10">
       {children}
     </section>
   )

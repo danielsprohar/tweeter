0b175e7c803b455579e739fcbf06b7ec90c76605
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-07-30 16:35:07 -0500

    MenuItem: css changes

--- src/app/components/menu/MenuItem.tsx
+++ src/app/components/menu/MenuItem.tsx
@@ -2,7 +2,7 @@ import { Children } from '../../types/children'
 
 export default function MenuItem({ children }: Children) {
   return (
-    <li className="whitespace-nowrap flex items-center gap-x-2 px-3 py-2 hover:bg-black/10 dark:hover:bg-white/10">
+    <li className="whitespace-nowrap flex items-center gap-x-2 px-3 py-2 bg-white text-black dark:bg-black dark:text-white z-50 hover:bg-black/10 dark:hover:bg-white/10">
       {children}
     </li>
   )

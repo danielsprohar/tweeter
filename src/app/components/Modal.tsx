eab65b973e221f334c018cc49e731e9c1e0479bb
Daniel Sprohar <daniel.sprohar@gmail.com>
2023-07-30 10:02:01 -0500

    ui: gave the Modal component a set width and height for medium+ screens

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -8,10 +8,10 @@ export default function Modal({ children }: ModalProps) {
   return (
     <div className="fixed flex flex-col items-center w-full h-full left-0 top-0 backdrop-contrast-50 z-10">
       <div className="flex justify-center h-screen w-screen">
-        <div>
+        <div className="flex flex-col items-center justify-center">
           <div
             role="dialog"
-            className="rounded-xl bg-white dark:bg-black z-10 w-full md:w-[600px]"
+            className="overflow-hidden rounded-xl bg-white dark:bg-black z-10 w-full md:w-[600px] md:h-[600px]"
           >
             {children}
           </div>
